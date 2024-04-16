3c890c24bd35839598d74e26b8c6b9ca80481240
val <79033705062@yandex.ru>
2024-04-16 13:33:11 +0300

    new

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import QtWidgets
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Start_menu(QtWidgets.QMainWindow):
+    ...
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = QtWidgets.QApplication([])
+main=Start_menu()
+main.show()
+sys.exit(app.exec_())
